3378ceffc2b9174784f7bf5bc0d200e8094ef283
ManuelZapater <manuelzb20@gmail.com>
2024-09-25 14:41:29 -0700

    Edited data structure

--- src/main/resources/db/migration/V20240908000002__entries.sql
+++ src/main/resources/db/migration/V20240908000002__entries.sql
@@ -1,5 +1,6 @@
 ALTER TABLE 'entries'
-    ADD COLUMN `username` VARCHAR(50) NULL,
-    ADD COLUMN `share_data` VARCHAR(1) NULL,
-    ADD COLUMN `date_of_birth` VARCHAR(20) NULL
+    ADD COLUMN `username` VARCHAR(50)           NOT NULL,
+    ADD COLUMN `share_data` VARCHAR(1)          NOT NULL,
+    ADD COLUMN `date_of_birth` VARCHAR(20)      NOT NULL
+
 
